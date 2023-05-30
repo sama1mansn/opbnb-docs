648a61b7edc5592bb529a18e2a7d992c101c9801
RumeelHussainbnb <93580180+RumeelHussainbnb@users.noreply.github.com>
2023-05-30 14:11:37 +0500

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,10 +14,10 @@ const config = {
   url: 'https://your-docusaurus-test-site.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/opbnb-docs',
 
   // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
+  // If you aren't using GitHub pages, you don't need these./
   organizationName: 'bnb-chain', // Usually your GitHub org/user name.
   projectName: 'opbnb-docs', // Usually your repo name.
 
